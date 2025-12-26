ebc1a01074e2ee7a2175c8ee701d2da34f630e4b
bachdd22 <144103691+bachdd22@users.noreply.github.com>
2025-12-26 21:00:01 +0700

    Update config.py

--- diffusers_implementation/config.py
+++ diffusers_implementation/config.py
@@ -5,11 +5,14 @@ def get_args():
     parse = argparse.ArgumentParser()
     
     # hyperparameter of StyleID
-    parse.add_argument('--T', type=int, default=1.5)
-    parse.add_argument('--gamma', type=int, default=0.75)
+    parse.add_argument('--T', type=float, default=1.5)
+    parse.add_argument('--gamma', type=float, default=0.75) # Changed to float for precision
     parse.add_argument('--without_init_adain', action='store_true')
     parse.add_argument('--without_attn_injection', action='store_true')
     parse.add_argument('--layers', nargs='+', type=int, default=[7, 8, 9, 10, 11])
+    
+    # NEW: Tunable AdaIN strength
+    parse.add_argument('--adain_strength', type=float, default=1.0, help="Strength of initial latent AdaIN (0.0 to 1.0)")
     
     # hyperparameters of diffusion model
     parse.add_argument('--ddim_steps', type=int, default=20)
